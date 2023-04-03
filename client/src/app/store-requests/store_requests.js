8fdada5a2f8b96fdcc4476c1fb2ad07e89170f52
Han <79167052+hannert@users.noreply.github.com>
2023-04-03 01:11:07 -0400

    backend woes Vi

--- client/src/app/store-requests/store_requests.js
+++ client/src/app/store-requests/store_requests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 const api = axios.create({
     baseURL: "https://mapperman.herokuapp.com/api", //our server we are deploying on
 })
