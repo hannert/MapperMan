1239838a6251101ccbb978791ed3d1fb67324ac0
cshinny01 <cshin01@hotmail.com>
2023-04-09 17:40:46 -0400

    Comments section done, time to work on css

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -2,3 +2,14 @@ import { Box, TextField, Typography } from "@mui/material";
 
 // import {GlobalStoreContext } rom '../store';
 
+export default function CommentCard(props){
+    const { comment } = props;
+    return (
+        <Box sx= {{backgroundColor: '#FFFFFF', color: 'black', display: 'flex', height:'fit'}}>
+            <Typography sx = {{margin:'5px', fontSize: 18}}>
+                {comment.comment}
+            </Typography>
+        </Box>
+    )
+}
+
--- client/src/components/CommentsList.js
+++ client/src/components/CommentsList.js
@@ -1,32 +1,40 @@
 import { Box, TextField, Typography } from "@mui/material";
 import { Grid, Button } from '@mui/material';
+import CommentCard from './CommentCard';
+
 
 export default function CommentsList(){
     const currentList = [
         {comment: "Woah, Cool!", index: 0},
         {comment: "Meh, mid", index: 1},
         {comment: "STOPPPP", index: 2},
-        {comment: "EWWW WHAT IS THIS MAP LOL", index: 3}
+        {comment: "EWWW WHAT IS THIS MAP LOL", index: 3},
+        {comment: "Hode up... kinda cool >_<", index: 4}
     ]
     return(
         <Box sx={{
-            height:'92.5%', 
             backgroundColor:'#D4D4F5', 
-            display:'flex', 
-            flexDirection:'column',
-            borderRadius: 2
             }}>
             <Typography variant="overline">Comments</Typography>
             <Box id='comment-container' sx={{height:'87.5%'}}>
-            </Box>
-            <Box sx={{p:1}}>
-                
-                <TextField
-                label="Add Comment"
-                >
+                <Box sx={{p:1}}>
+                    <Grid container rowSpacing={6} columnSpacing={6}>
+                            {      
+                            currentList.map((comment)=>(
+                                <Grid item xs = {12} sx={{align: 'center'}} >
+                                    <CommentCard key={comment} sx = {{height: '400px', backgroundColor: '#282c34'}} comment={comment}/>
+                                </Grid>
+                            ))
+                        }
+                    </Grid>
+                    <TextField
+                    label="Add Comment"
+                    >
 
-                </TextField>
+                    </TextField>
+                </Box>
             </Box>
+            
 
         </Box>
     )
