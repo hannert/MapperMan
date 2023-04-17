1e23e67a16d332eb398a5bde9b79e6713f651131
cshinny01 <cshin01@hotmail.com>
2023-04-17 17:53:36 -0400

    tests done, will change the url back to the actual site when deployment ready

--- client/src/components/LoginScreen.js
+++ client/src/components/LoginScreen.js
@@ -46,26 +46,10 @@ export default function LoginScreen() {
     const handleSubmit = (event) => {
         event.preventDefault();
         const formData = new FormData(event.currentTarget);
-        
-        apis.loginUser(
-                formData.get('email'),
-                formData.get('password')
-            ).then((response) => {
-                if(response.data.success === true){
-                    console.log("Login successful");
-                    console.log(response.data.user);
-                    dispatch(loginUser({
-                        user: response.data.user,
-                        loggedIn: true,
-                    }))
-                    navigator('/maps')
-                }else{
-                    console.log("Login failed");
-                    console.log(response.data);
-                }
-            })
-        };
-        
+        const email = formData.get('email');
+        const password = formData.get('password');
+        auth.loginUser(email, password);
+    }
     
 
     return (
@@ -117,6 +101,7 @@ export default function LoginScreen() {
                             type="submit"
                             fullWidth
                             variant="contained"
+                            id="sign-in-button"
                             sx={{ mt: 3, mb: 2 }}
                         >
                             Sign In
