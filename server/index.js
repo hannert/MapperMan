ac9dcee3d5039a52048c3849cc0ad8ac6cd7550a
Kevin Ha <kevinha159@gmail.com>
2023-04-17 00:00:01 -0400

    Manually set cors

--- server/index.js
+++ server/index.js
@@ -10,24 +10,20 @@ const PORT = process.env.PORT || 4000;
 const app = express()
 
 // SETUP THE MIDDLEWARE
-app.use(express.urlencoded({limit: '2000kb', extended: true, parameterLimit:50000}));
-app.use(cors({
-    origin: ['localhost:3000'],
-    credentials: false
 
-}))
+app.use(express.urlencoded({limit: '2000kb', extended: true, parameterLimit:50000}));
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-    );
-    next();
-  });
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, PUT, DELETE, OPTIONS"
+  );
+});
+
+
 app.use(express.json({limit:'2000kb'}))
 app.use(cookieParser())
 
