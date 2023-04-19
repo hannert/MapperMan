88c5264bfe8bbded3bfcbf43efb1997d6832a38f
Han <79167052+hannert@users.noreply.github.com>
2023-04-19 18:17:10 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -45,4 +45,4 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 // PUT THE SERVER IN LISTENING MODE
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
 
-
+// :3
