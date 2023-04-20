39b83c9788aafe5bacdf5be344b079276739da41
Han <79167052+hannert@users.noreply.github.com>
2023-04-20 14:11:18 -0400

    Update auth_requests.js
    Changed credentials for now to test CORS

--- client/src/app/store-requests/auth_requests.js
+++ client/src/app/store-requests/auth_requests.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 const api = axios.create({
     baseURL: process.env.REACT_APP_AUTH_URL,
 })
