7f6207c22e04cfc241ec6e4bec505a71e5e445dc
cshinny01 <cshin01@hotmail.com>
2023-04-24 18:54:54 -0400

    Removed button

--- client/src/components/ForgotPasswordScreen.js
+++ client/src/components/ForgotPasswordScreen.js
@@ -80,14 +80,6 @@ export default function ForgotPasswordScreen() {
                             autoComplete="email"
                             autoFocus
                         />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Verify Email
-                        </Button>
                         <TextField
                             margin="normal"
                             required
