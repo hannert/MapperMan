3e559df41240c8d0768b1f03fb0f15b2ee16e6bd
Han <79167052+hannert@users.noreply.github.com>
2023-04-24 16:07:38 -0400

    Backend-Tests

--- server/tests/getmap.test.js
+++ server/tests/getmap.test.js
@@ -1,23 +1,23 @@
 
-// const request = require('supertest');
+const request = require('supertest');
 
-//  describe('GET /map/:id', function(){
-//     it('Should get this map', function(done){
-//       request('https://mapperman.herokuapp.com/api')
-//           .get('/map/642a76b1a946889825d2cf1f')
-//           .expect(200)
-//           .end(function(err, res) {
-//             if (err) return done(err);
-//             return done();
-//           });
-//     }) 
-//     it('Should not find a map', function(done){
-//       request('https://mapperman.herokuapp.com/api')
-//           .get('/map/kasdfkjasgdfkjhasgdflkajsdfh')
-//           .expect(400)
-//           .end(function(err, res) {
-//             if (err) return done(err);
-//             return done();
-//           });
-//     }) 
-// })
+ describe('GET /map/:id', function(){
+    it('Should get this map', function(done){
+      request('https://mapperman.herokuapp.com/api')
+          .get('/map/6446c521d2b32508b63f2a28')
+          .expect(200)
+          .end(function(err, res) {
+            if (err) return done(err);
+            return done();
+          });
+    }) 
+    it('Should not find a map', function(done){
+      request('https://mapperman.herokuapp.com/api')
+          .get('/map/kasdfkjasgdfkjhasgdflkajsdfh')
+          .expect(400)
+          .end(function(err, res) {
+            if (err) return done(err);
+            return done();
+          });
+    }) 
+})
